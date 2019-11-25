2ce51f3676b541f8241949522c5484ff1b0b7782
chaofiber <chao@LAPTOP-INLO0HFG.localdomain>
2019-11-25 21:13:41 +0100

    add framework

--- ComputeTransitionProbabilities.m
+++ ComputeTransitionProbabilities.m
@@ -29,6 +29,73 @@ global GAMMA R P_WIND
 global FREE TREE SHOOTER PICK_UP DROP_OFF BASE
 global NORTH SOUTH EAST WEST HOVER
 global K TERMINAL_STATE_INDEX
+% find the treeList (number of tree * 1)
+treeList = findTree(map);
+num_tree = size(treeList,1);
+m, n = size(map);
+P = zeros(K,K,5);
 
+for i = 1:K
+    % Possible position can be reached from state i stateSpace(i) = (i,0/1)
+    %                        I
+    %                      F G H
+    %                    A B i D E 
+    %                      J K L
+    %                        M
+    
+    % CASE ONE: WEST
+    
+    % CASE TWO: SOUTH
+    
+    % CASE THREE: NORTH
+    
+    % CASE FOUR: EAST
+    
+    % CASE FIVE: HOVER
+    
+    posI_x, posI_y, state = stateSpace(i,:,:);
+    posI_A = [posI_x, pos_y-2];
+    idxI = n*posI_x + posI_y;
+    idxA = n*posI_x + posI_y;
+    % Compute number of trees between i and A: p 
+    
+end
+    
 
+end
+
+function treeList = findTree(map)
+% return an array treeList (number of tree * 1)
+global TREE
+treeList = [ ];
+for i = 1:size(map,1)
+    for j = 1:size(map,2)
+        if map(i,j) == TREE
+            treeList = [treeList;
+                        i*n+j];
+        end
+
+    end
+end
+end
+
+function idx = binarySearch(A, num)
+% return the idx of a cell in the treeList
+l = 1;
+r = length(A);
+   idx = 1;
+   while l < r
+      idx = 1 + floor((l + r - 1) / 2);
+      if A(idx) > num
+        r = idx - 1; 
+      elseif A(idx) <= num
+        l = idx;
+      end
+   end
+   if l == r
+      idx = r; 
+   end
+   if A(idx) > num
+     idx = -1;
+   end
 end
