2b0efdb8b02e42105f664245375e82048cb62328
fangzl123 <fang@DESKTOP-2GR3SUR.localdomain>
2019-12-17 20:52:38 +0100

    final version

--- ComputeTransitionProbabilities.m
+++ ComputeTransitionProbabilities.m
@@ -29,19 +29,22 @@ global GAMMA R P_WIND
 global FREE TREE SHOOTER PICK_UP DROP_OFF BASE
 global NORTH SOUTH EAST WEST HOVER
 global K TERMINAL_STATE_INDEX
-global L
-%global BASE_STATE_INDEX
 
 %% INITILIZATION
-% find the treeList (number of tree * 1)
-global m n
+global m n L
 [m, n] = size(map);
 L = 5;
+% find the position of all shooters and compute a probability matrix of
+% surviving, which will be multiplied in the calculation of transition
+% probabilities matrix
 shooterList = findShooter(map);
 Prob_Survive = Survive(shooterList, stateSpace);
+
+% find the state index of every cell in the map
 idxList = MaptoIndex(stateSpace);
+% define the change of coordinates only by inputs or winds of different direction
 Direction = zeros(5,2);
-Direction(EAST,:) = [1, 0];
+Direction(EAST,:) = [1,0];
 Direction(WEST,:) = [-1,0];
 Direction(NORTH,:) = [0,1];
 Direction(SOUTH,:) = [0,-1];
@@ -54,21 +57,23 @@ BASE_STATE_INDEX = find(ismember(stateSpace,[x,y,0],'rows'));
 PICK_UP_INDEX = find(ismember(stateSpace,[x,y,1],'rows'));
 
 P = zeros(K,K,L);
+% define a matrix that shows the probability of return to the base due to
+% the crash of each state
 P_IMNORMAL_TO_BASE = zeros(K,L);
+
 %% COMPUTE ODD SUB TRANSITION MATRIX FIRST
 for i = 1:2:K
     pos_i = stateSpace(i,1:2);
     for action = [WEST, SOUTH, NORTH, EAST, HOVER]
         % Check if this action is allowed ( NOT HITTING A TREE AND
         % NOT OUT OF BORDER !!!)
-        % ppos: the cell the drone is supposed to be in if no wind
-        % happens
+        % ppos: the cell the drone is supposed to be in if no wind happens
         ppos = pos_i + Direction(action,:);
         if ~OutOfBorder(m,n,ppos)
             j_temp = idxList(ppos(1), ppos(2));
             if j_temp ~= -1
-                % This action is valid only if it neither hit the
-                % tree nor hit the border
+                % This action is valid only if it neither hits the
+                % tree nor hits the border
                 p_normal = 0;
                 % WIND COMES!!!!!
                 for wind = [WEST, SOUTH, NORTH, EAST]   
@@ -76,8 +81,9 @@ for i = 1:2:K
                     if ~OutOfBorder(m,n,pos_j)
                         j = idxList(pos_j(1), pos_j(2));
                         if j ~= -1 % NOT A TREE
-                            P_temp = P_WIND*0.25; 
-                            P(i, j, action) = P_temp*Prob_Survive(pos_j(1), pos_j(2));
+                            P_temp = P_WIND * 0.25; 
+                            P(i, j, action) = P_temp * Prob_Survive(pos_j(1), pos_j(2));
+                            % copy the result to the cell with even state index
                             P(i+1, j+1, action) = P(i, j, action);
                             p_normal = p_normal + P(i,j,action);
                         end
@@ -85,10 +91,11 @@ for i = 1:2:K
                 end
                 % LUCKY! NO WIND!!! STAY WHERE YOU ARE!
                 P_temp = 1 - P_WIND;
-                P(i,j_temp,action) = P_temp*Prob_Survive(ppos(1), ppos(2));
+                P(i,j_temp,action) = P_temp * Prob_Survive(ppos(1), ppos(2));
+                % copy the result to the cell with even state index
                 P(i+1, j_temp+1, action) = P(i, j_temp, action);
                 p_normal = p_normal + P(i,j_temp,action);
-                % OTHERWISE, GO BACK TO BASE! -.-
+                % OTHERWISE, GO BACK TO BASE! -.- CRASH!!
                 P_IMNORMAL_TO_BASE(i, action) = 1 - p_normal;
             end
             
@@ -97,38 +104,34 @@ for i = 1:2:K
 
     
 end
-% COPY TO THE EVEN INDEX SUB TRANSITION MATRIX
-% TO DO: 1. copy the odd index pair to the even index pair (i,j,u) 
-% 2. consider following cases: 
+
+% correct some trivial errors in simple copying
+% consider following cases: 
 % from FREE WITHOUT PACKAGE to PICKUP;
-% from FREE WITH PACKAGE to BASE (lose one's package);
+% from FREE WITH PACKAGE to BASE (crash and lose its package);
 % from FREE WITH PACKAGE to DROPOFF
 % IT IS IMPOSSIBLE THAT ONE IS IN PICKUP WITHOUT A PACKAGE. 
-% Or just add P(i+1,j+1) after each line above in the odd case.
-% for i = 2:2:K
-%     for action = [WEST, SOUTH, NORTH, EAST, HOVER]
-%         P(i,2:2:K,action) = P(i-1,1:2:K,action);
-%     end
-% end
 
-% FROM FREE WITHOUT PACKAGE to PICKUP  PICK_UP_INDEX: even
-%for i = [Reachable(PICK_UP_INDEX) ]
+
+% FROM FREE WITHOUT PACKAGE to PICKUP  
+% correct the probability of transition from other states to pickup state
+% since PICK_UP index is even
 P(1:2:K, PICK_UP_INDEX, :) = P(1:2:K, PICK_UP_INDEX-1,:);
 P(1:2:K, PICK_UP_INDEX-1, :) = 0;
-%end
+
 % FROM FREE WITH PACKAGE to BASE: can be from anywhere with even index,
-% just the probability of being crashed.
+% it is just the probability of being crashed.
 for i = 2:2:K
-    idx = BASE_STATE_INDEX;
-    P(i, idx, :) = P_IMNORMAL_TO_BASE(i-1, :);
+    P(i, BASE_STATE_INDEX, :) = P_IMNORMAL_TO_BASE(i-1, :);
 end
 
-% UPDATE THE PROB TO BASE
+% UPDATE THE PROBABILITY TO BASE
 for i = 1:2:K
     for action = [WEST, SOUTH, NORTH, EAST, HOVER]
         P(i,BASE_STATE_INDEX,action) = P(i, BASE_STATE_INDEX,action) + P_IMNORMAL_TO_BASE(i,action);
     end
 end
+
 % FROM FREE WITH PACKAGE TO DROPOFF: DON'T HAVE TO CHANGE!!!
 % FROM TERMINAL_STATE_INDEX: STAY THERE!
 P(TERMINAL_STATE_INDEX, :, :) = 0;
@@ -181,7 +184,8 @@ end
 end
 
 function indexList = MaptoIndex(stateSpace)
-% return a matrix (m * n), indexList(i,j) = stateIndex
+% return a matrix (m * n), indexList(i,j) = stateIndex of position(i,j),
+% indexList(i,j)= -1 means there is a tree in this position
 global K m n
 indexList = -1*ones(m,n);
 for i = 1:2:K
